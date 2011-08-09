975154f5d7e6d5bd302292591e8952c60b18ce72
Adrian Mowat <adrian.mowat@gmail.com>
2011-08-09 07:01:06 +0100

    Added every function

--- lib/rawk/rawk.rb
+++ lib/rawk/rawk.rb
@@ -1,16 +1,24 @@
 module Rawk
   class Program
     def initialize(input_stream)
-      @start = []
+      @start, @every = [], []
+      @input_stream = input_stream
     end
     
     def start(block)
       @start << block unless @start.include? block
     end
     
+    def every(block)
+      @every << block unless @every.include? block
+    end
+    
     def run(&block)
       instance_eval(&block)
       @start.each {|b| b.call}
+      @input_stream.each_line do |line|
+        @every.each {|b| b.call}
+      end
     end
   end
 end
--- spec/rawk/rawk_spec.rb
+++ spec/rawk/rawk_spec.rb
@@ -7,15 +7,8 @@ module Rawk
         @data = "a b\nc d\ne f\n"
         @program = Program.new(@data)
       end
-      
-      it "calls a single start block once" do
-        start_block = mock("start block")
-        start_block.should_receive(:call).once
-        @program.run do
-          start(start_block)                    
-        end
-      end
-      
+
+      # Start -----------            
       it "calls each of many start blocks once" do
         start_block1 = mock("start block 1")
         start_block2 = mock("start block 2")
@@ -26,7 +19,6 @@ module Rawk
           start(start_block2)                    
         end
       end
-      
       it "only calls a duplicate start block once" do
         start_block = mock("start block")
         start_block.should_receive(:call).once
@@ -34,6 +26,27 @@ module Rawk
           start(start_block)                    
           start(start_block)                    
         end        
+      end
+
+      # During -----------      
+      it "calls all the every blocks n times with each line of data" do
+        every1 = mock("every 1")
+        every2 = mock("every 2")
+        every1.should_receive(:call).exactly(3).times
+        every2.should_receive(:call).exactly(3).times
+        @program.run do
+          every(every1)
+          every(every2)
+        end
+      end
+      
+      it "calls duplicate every blocks only once for each line" do
+        every = mock("every block")
+        every.should_receive(:call).exactly(3).times
+        @program.run do
+          every(every)
+          every(every)
+        end
       end
     end
   end
